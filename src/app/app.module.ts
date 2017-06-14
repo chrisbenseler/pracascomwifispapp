2702aa5402b3f1c661ef9627e75486cefa6fe972
Christian <cbenseler@gmail.com>
2017-06-14 14:09:25 -0300

    chore: version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { HttpModule, Http } from '@angular/http';
+import { HttpModule } from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
--- src/pages/zones/zones.ts
+++ src/pages/zones/zones.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 import { ModalController } from 'ionic-angular';
 
 import { PlazasProvider } from '../../providers/plazas/plazas';
@@ -28,7 +28,7 @@ export class ZonesPage {
   }
 
   ionViewDidLoad() {
-    
+
     this.plazasProvider.all().subscribe( plazas => {
 
       plazas.forEach( plaza => {
